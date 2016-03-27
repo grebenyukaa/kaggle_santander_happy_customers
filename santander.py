667cd5dbcb28f321f787a4148b7689fab6bd56e0
amadeus <am-vi@yandex.ru>
2016-03-27 23:00:28 +0300

    0.51

--- santander.py
+++ santander.py
@@ -23,11 +23,11 @@ import pylab as pl
 
 # In[3]:
 
-from pandas import read_csv, DataFrame, Series
+from pandas import read_csv, to_csv, DataFrame, Series
 data = read_csv('~/kaggle/santander/train.csv')
 
 
-# In[23]:
+# In[32]:
 
 #throw away features, where >= 80% of values is equal to median
 cdata = data
@@ -35,23 +35,23 @@ for col in cdata.columns[1:-1]:
     ser = cdata[col]
     cnt = ser.value_counts()
     med = ser.median()
-    if cnt[med]/len(ser) >= 0.60:
+    if cnt[med]/len(ser) >= 0.80:
         cdata = cdata.drop([col], axis=1)
-#cdata
+#cdata = data
 
 
-# In[24]:
+# In[33]:
 
 print(len(cdata.columns))
 
 
-# In[25]:
+# In[34]:
 
 #for col in cdata.columns[1:-1]:
 #    cdata.pivot_table('ID', [col], 'TARGET', 'count').plot(title=col)
 
 
-# In[26]:
+# In[35]:
 
 Y = cdata['TARGET']
 X = cdata.drop(['ID', 'TARGET'], axis=1)
@@ -59,12 +59,12 @@ kcv = 10
 cvd = {}
 
 
-# In[27]:
+# In[36]:
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 
-# In[28]:
+# In[37]:
 
 model_rfc = RandomForestClassifier(n_estimators = 20)
 #model_knc = KNeighborsClassifier(n_neighbors = 18)
@@ -72,31 +72,31 @@ model_rfc = RandomForestClassifier(n_estimators = 20)
 #model_svc = svm.SVC()
 
 
-# In[29]:
+# In[38]:
 
 rf_scores = cross_validation.cross_val_score(model_rfc, X_train, y_train, cv = kcv)
 cvd['RandomForestClassifier'] = rf_scores.mean()
 
 
-# In[30]:
+# In[39]:
 
 #kn_scores = cross_validation.cross_val_score(model_knc, X_train, y_train, cv = kcv)
 #cvd['KNeighborsClassifier'] = kn_scores.mean()
 
 
-# In[31]:
+# In[40]:
 
 #lr_scores = cross_validation.cross_val_score(model_lr, X_train, y_train, cv = kcv)
 #cvd['LogisticRegression'] = lr_scores.mean()
 
 
-# In[32]:
+# In[41]:
 
 #svc_scores = cross_validation.cross_val_score(model_svc, X_train, y_train, cv = kcv)
 #cvd['SVC'] = svc_scores.mean()
 
 
-# In[33]:
+# In[42]:
 
 print(rf_scores)
 #print(kn_scores)
@@ -106,7 +106,7 @@ print(rf_scores)
 #plt.plot(range(kcv), rf_scores, 'r', range(kcv), kn_scores, 'g', range(kcv), lr_scores)#, 'b', range(kcv), svc_scores, 'c')
 
 
-# In[34]:
+# In[43]:
 
 pl.clf()
 
@@ -134,6 +134,28 @@ pl.xlabel('False Positive Rate')
 pl.ylabel('True Positive Rate')
 pl.legend(loc=0, fontsize='small')
 pl.show()
+
+
+# In[56]:
+
+testdata = read_csv('~/kaggle/santander/test.csv')
+ctestdata = testdata
+for col in testdata.columns:
+    if col not in cdata.columns:
+        ctestdata = ctestdata.drop([col], axis=1)
+ctestdata = ctestdata.drop(['ID'], axis=1)
+
+
+# In[62]:
+
+result = DataFrame(testdata['ID'])
+result.insert(1,'TARGET', model_rfc.fit(X, Y).predict(ctestdata))
+result.to_csv('~/ml/kaggle_santander_happy_customers/res.csv', index=False)
+
+
+# In[63]:
+
+len(result)
 
 
 # In[ ]:
